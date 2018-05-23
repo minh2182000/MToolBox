8613969938302458219f1fb3c068f409c9f8df37
Minh Pham <minhpham@mail.usf.edu>
2018-05-23 00:39:34 -0400

    5/23 Update

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,3 +1,9 @@
+#' @import installr
 .onAttach <- function(libname, pkgname){
-  packageStartupMessage("Start")
+  Version = packageVersion("MToolBox")
+  packageStartupMessage(sprintf("MToolBox %s", Version))
+  readline("Do you want to check for R update? (y/n) ") -> answer
+  if (answer == "y"){installr::updateR()
+  } else if (answer != "n"){packageStartupMessage("Do not regconize response, not checking for update")
+    }
 }
