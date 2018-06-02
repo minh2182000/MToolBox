b0f147483ba75f710510aefec2d4f0c37b4a57d8
Minh Pham <minhpham@mail.usf.edu>
2018-06-02 10:26:23 -0400

    6/2 Update

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,15 @@
+.onLoad = function(libname, pkgname){
+  print("LOADING MTOOLBOX")
+  if (! "car" %in% rownames(installed.packages()))
+    install.packages("car")
+  if (! "installr" %in% rownames(installed.packages()))
+    install.packages("installr")
+  if (! "mctest" %in% rownames(installed.packages()))
+    install.packages("mctest")
+  if (! "plotly" %in% rownames(installed.packages()))
+    install.packages("plotly")
+}
+
 #' @import installr
 .onAttach <- function(libname, pkgname){
   Version = packageVersion("MToolBox")
