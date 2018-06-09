fdcaa875c2560d680ced1fc02085ce466f8ff877
Minh Pham <minhpham@mail.usf.edu>
2018-06-09 03:19:04 -0400

    6/9/2018 Update

--- R/Plot.R
+++ R/Plot.R
@@ -1,14 +1,15 @@
 #' Save Publication-quality plot
+#' @description Savev a plot in a high-quality jpeg file for publication
 #' @param plot any plot or ggplot object
-#' @param res resolution
-#' @param height height
-#' @param width width
+#' @param res resolution (dpi)
+#' @param height height (in pixels)
+#' @param width width (in pixels)
 #' @param dir saving directory
 #' @param filename file name
 #' @export
 
-Plot.PublishPlot = function(plot,  res = 400, height = 1500, width = 3000, dir = getwd(), filename = "Plot Output"){
-  jpeg(filename = paste0(dir, "/", filename, ".jpg"), res, height, width)
+Plot.PublishPlot = function(plot,  res = 600, height = 2000, width = 4000, dir = getwd(), filename = "Plot Output"){
+  jpeg(filename = paste0(dir, "/", filename, ".jpg"), res =  res, height = height, width = width)
   plot; dev.off()
   graphics.off()
 }
@@ -17,9 +18,14 @@ Plot.PublishPlot = function(plot,  res = 400, height = 1500, width = 3000, dir =
 
 
 #' Visualize High-dimensional data in a 3D scatter plot
-#' Using PCA to visualize a classification data at 2D or 3D
+#' @description Use dimension reduction (Principle Component Analysis) to visualize a classification/regression data in a 2D or 3D plot
+#' @param formula regression or classification formula. If classification, the left hand side must be a factor or character. If regression, the left hand side must be numeric.
+#' @param data Data Frame or matrix for the formula
+#' @param dim Dimmension to be plotted. If null, will plot 2D if only 2 predictors, 3D otherwise.
 #' 
 #' @import plotly
+#' @import ggplot2
+#' @return A list of: \code{Plot}: the plot, \code{VarExplained}: proportion of variance explained by each PC, \code{TotalVarExplained}: total proportion of variance explained by PCs
 #' @export
 Plot.VisualizeSupervise = function(formula, data, dim = NULL){
   y = data[all.vars(formula)[1]]
@@ -28,20 +34,40 @@ Plot.VisualizeSupervise = function(formula, data, dim = NULL){
   
   # PCA
   PCA = prcomp(x)
-  VarExplained = (PCA$sdev^2/sum(PCA$sdev^2))[1:dim]
-  PCs = PCA$x[,1:dim]
   
   # plot
-  Group = y; colnames(Group) = "Group"
-  PlotData = as.data.frame(cbind(PCs, Group)); PlotData$Group = as.factor(PlotData$Group)
-  if (dim == 3){
-    Plot = plot_ly(data = PlotData, x = ~PC1, y = ~PC2, z = ~PC3, color = ~Group,
-                   colors = "Set1") %>%
-          add_markers(marker = list(size = 5))
+  if (is.factor(y) | is.character(y)){
+    # Classfication
+    VarExplained = (PCA$sdev^2/sum(PCA$sdev^2))[1:dim]
+    PCs = PCA$x[,1:dim]
+    
+    Group = y; colnames(Group) = "Group"
+    PlotData = as.data.frame(cbind(PCs, Group)); PlotData$Group = as.factor(PlotData$Group)
+    if (dim == 3){
+      Plot = plot_ly(data = PlotData, x = ~PC1, y = ~PC2, z = ~PC3, color = ~Group,
+                     colors = "Set1") %>%
+        add_markers(marker = list(size = 5))
+    }
+    if (dim == 2){
+      Plot = ggplot(data = PlotData, aes(x = PC1, y = PC2, col = Group)) +
+        geom_point()
+    }
+  }else if (is.numeric(y)) {
+    # Regression
+    VarExplained = (PCA$sdev^2/sum(PCA$sdev^2))[1:(dim-1)]
+    PCs = PCA$x[,1:(dim-1)]
+    
+    PlotData = as.data.frame(cbind(PCs, y))
+    if (dim == 3){
+      Plot = plot_ly(data = PlotData, x = ~PC1, y = ~PC2, z = ~y) %>%
+        add_markers(marker = list(size = 5))
+    }
+    if (dim == 2){
+      Plot = ggplot(data = PlotData, aes(x = PC1, y = y)) +
+        geom_point()
+    }
+    
   }
-  if (dim == 2){
-    Plot = ggplot(data = PlotData, aes(x = PC1, y = PC2, col = Group)) +
-      geom_point()
-  }
-  return(list(Plot = Plot, VarExplained = VarExplained))
+  
+  return(list(Plot = Plot, VarExplained = VarExplained, TotalVarExplained = sum(VarExplained)))
 }
