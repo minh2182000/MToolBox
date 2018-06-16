888c19679063c99f9c1912f27586f90fa17ac767
Minh Pham <minhpham@mail.usf.edu>
2018-06-16 11:01:43 -0400

    6/16 Update

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,24 @@
 .onLoad = function(libname, pkgname){
   print("LOADING MTOOLBOX")
-  if (! "car" %in% rownames(installed.packages()))
-    install.packages("car")
+  get_os <- function(){
+    sysinf <- Sys.info()
+    if (!is.null(sysinf)){
+      os <- sysinf['sysname']
+      if (os == 'Darwin')
+        os <- "osx"
+    } else { ## mystery machine
+      os <- .Platform$OS.type
+      if (grepl("^darwin", R.version$os))
+        os <- "osx"
+      if (grepl("linux-gnu", R.version$os))
+        os <- "linux"
+    }
+    tolower(os)
+  }
+
+  if (!get_os() == "linux")
+    if (! "car" %in% rownames(installed.packages()))
+      install.packages("car")
   if (! "installr" %in% rownames(installed.packages()))
     install.packages("installr")
   if (! "mctest" %in% rownames(installed.packages()))
@@ -14,14 +31,30 @@
 .onAttach <- function(libname, pkgname){
   Version = packageVersion("MToolBox")
   packageStartupMessage(sprintf("MToolBox %s", Version))
-  
-  Newer_version = installr::check.for.updates.R(notify_user = FALSE)
-  if (Newer_version){
-    readline("There is a newer version of R, would you like to update R? (y/n) ") -> answer
-    if (answer == "y"){installr::updateR()
-    } else if (answer != "n") packageStartupMessage("Do not regconize response, abort")
-    
+
+  get_os <- function(){
+    sysinf <- Sys.info()
+    if (!is.null(sysinf)){
+      os <- sysinf['sysname']
+      if (os == 'Darwin')
+        os <- "osx"
+    } else { ## mystery machine
+      os <- .Platform$OS.type
+      if (grepl("^darwin", R.version$os))
+        os <- "osx"
+      if (grepl("linux-gnu", R.version$os))
+        os <- "linux"
+    }
+    tolower(os)
   }
-  
+  if (!get_os() == "linux"){
+    Newer_version = installr::check.for.updates.R(notify_user = FALSE)
+    if (Newer_version){
+      readline("There is a newer version of R, would you like to update R? (y/n) ") -> answer
+      if (answer == "y"){installr::updateR()
+      } else if (answer != "n") packageStartupMessage("Do not regconize response, abort")
+
+    }
+  }
   
 }
